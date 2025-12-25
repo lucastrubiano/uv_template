98fbc86509ca0cd40b082f068b4b99fa286e3cd8
Lucas Trubiano <lucastrubiano@Mac-mini-de-Lucas.local>
2025-12-25 13:33:07 -0300

    feat: entornos efimeros

---
+++ scripts/api1.py
@@ -0,0 +1,17 @@
+# /// script
+# requires-python = ">=3.13, <3.14"
+# dependencies = [
+#     "boto3",
+#     "requests"
+# ]
+# ///
+
+import boto3
+import requests
+import sys
+
+# print python version
+print(sys.version)
+
+# ... resto del código
+# uv run api1.py
---
+++ scripts/api2.py
@@ -0,0 +1,17 @@
+# /// script
+# requires-python = ">=3.10, <3.11"
+# dependencies = [
+#     "boto3",
+#     "requests"
+# ]
+# ///
+
+import boto3
+import requests
+import sys
+
+# print python version
+print(sys.version)
+
+# ... resto del código
+# uv run api2.py
